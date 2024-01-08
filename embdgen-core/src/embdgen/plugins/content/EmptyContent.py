ce40c030b0317c7cf3bbdf2d028dfed503ffdeca
Joerg Vehlow <joerg.vehlow@aox.de>
2024-01-08 14:51:20 +0100

    EmptyContent: new module
    Allow adding a completely empty partition

---
+++ embdgen-core/src/embdgen/plugins/content/EmptyContent.py
@@ -0,0 +1,11 @@
+import io
+from io import BufferedIOBase
+
+from embdgen.core.content.BinaryContent import BinaryContent
+
+class EmptyContent(BinaryContent):
+    """Empty content"""
+    CONTENT_TYPE = "empty"
+
+    def do_write(self, file: BufferedIOBase):
+        file.seek(self.size.bytes, io.SEEK_CUR)
---
+++ embdgen-core/tests/content/test_EmptyContent.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+from embdgen.core.utils.SizeType import SizeType
+from embdgen.plugins.content.EmptyContent import EmptyContent
+
+def test_EmptyContent(tmp_path: Path):
+    image = tmp_path / "image.raw"
+
+    obj = EmptyContent()
+    obj.size = SizeType(2048)
+
+    obj.prepare()
+
+    assert obj.size.bytes == 2048
+
+    with image.open("wb+") as f:
+        obj.write(f)
+
+        assert f.tell() == 2048
+        # Allow EmptyContent to only seek, not write anything
+        f.write(b"\1")
+    
+    assert image.read_bytes() == (b"\0" * 2048) + b"\1"
+
